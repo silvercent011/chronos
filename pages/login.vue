74980a42f05e40ee9f7a1949db5b94838b04ad7a
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2023-05-25 15:30:48 -0300

    feat(auth): implementa autenticação com google

--- pages/login.vue
+++ pages/login.vue
@@ -5,6 +5,10 @@ function signInWithGitHub() {
   signIn("github");
 }
 
+function signInWithGoogle() {
+  signIn("google");
+}
+
 definePageMeta({
   layout: false,
 });
@@ -13,13 +17,26 @@ definePageMeta({
 <template>
   <v-container class="d-flex flex-column h-screen align-center justify-center">
     <h1>Chronos</h1>
-    <div>
+    <div class="d-flex flex-column btns">
       <v-btn @click="signInWithGitHub">
         <template #prepend>
           <v-icon icon="mdi-github"></v-icon>
         </template>
         Entrar com GitHub
       </v-btn>
+
+      <v-btn @click="signInWithGoogle">
+        <template #prepend>
+          <v-icon icon="mdi-google"></v-icon>
+        </template>
+        Entrar com Google
+      </v-btn>
     </div>
   </v-container>
 </template>
+
+<style scoped>
+.btns {
+  gap: 1rem;
+}
+</style>
--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,16 +1,19 @@
 import { NuxtAuthHandler } from "#auth";
 import GithubProvider from "next-auth/providers/github";
-
-const clientId = process.env.GITHUB_CLIENT_ID;
-const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+import GoogleProvider from "next-auth/providers/google";
 
 export default NuxtAuthHandler({
   secret: "test",
   providers: [
     // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
     GithubProvider.default({
-      clientId,
-      clientSecret,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    // @ts-expect-error
+    GoogleProvider.default({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
